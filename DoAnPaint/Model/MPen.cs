fe0e0b9f457181c0aa558199d0eb6edb84551be3
nguyen-hoang-nam123 <80116172+nguyen-hoang-nam123@users.noreply.github.com>
2023-03-16 13:24:48 +0700

    first commit

---
+++ DoAnPaint/Model/MPen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DoAnPaint.Model
+{
+    /// <summary>
+    /// Lớp dại diện cho đường vẽ bằng pencil
+    /// </summary>
+    public class MPen:MCurve
+    {
+        //TODO: cho biết chọn chế độ xóa hay là không
+        public bool isEraser { get; set; }
+
+        public MPen()
+        {
+            name = "Pen";
+        }
+
+        public MPen(Color color)
+        {
+            name = "Pen";
+            this.color = color;
+        }
+
+    }
+}
